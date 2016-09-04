74efb38c8bb88379bb244167f3eb65e3d930fa31
Adriel Klein <adrielmklein@gmail.com>
2016-09-04 16:26:58 -0700

    make it fail

--- tests/test_acknowledge_route.py
+++ tests/test_acknowledge_route.py
@@ -2,4 +2,4 @@ def test_when_acknowledge_route_is_hit_then_it_returns_ok(app):
     with app.test_client() as test_client:
         response = test_client.get('/acknowledge')
         assert 200 == response.status_code
-        assert 'OK' == response.get_data().decode()
+        assert 'OKasd' == response.get_data().decode()
