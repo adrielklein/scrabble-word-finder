41a9f3621acb47917e1d1bd5bda2850321a8c5f6
Adriel Klein <adrielmklein@gmail.com>
2016-09-06 11:14:17 -0700

    add route classes

--- app/main.py
+++ app/main.py
@@ -1,25 +1,19 @@
-import json
-
 from flask import Flask
 
 from app.anagram_finder import AnagramFinder
+from app.routes import AcknowledgeRoute, WordRoute
 from app.word_finder import WordFinder
 
 
-def handle_acknowledge_route():
-    return 'OK'
-
-
-def handle_word_route(letter_string):
-    anagram_finder = AnagramFinder([])
-    word_finder = WordFinder(anagram_finder)
-    result = {'words': word_finder.get_words(letter_string)}
-    return json.dumps(result)
+def _set_up_routes(routes, app):
+    for route in routes:
+        app.add_url_rule(route.path, route.endpoint, route.handle)
 
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule('/acknowledge', '', handle_acknowledge_route)
-    app.add_url_rule('/words/<letter_string>', 'word', handle_word_route)
+    word_finder = WordFinder(AnagramFinder([]))
+    routes = [AcknowledgeRoute(), WordRoute(word_finder)]
+    _set_up_routes(routes, app)
 
     return app
