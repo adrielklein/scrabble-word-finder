41b28c79cc15fe3e217ddede549f6f87e36c33f7
Adriel Klein <adrielmklein@gmail.com>
2016-09-08 14:59:42 -0700

    major redesign

--- app/static/word_retriever.js
+++ app/static/word_retriever.js
@@ -1,35 +1,63 @@
-function showWords() {
-    $("#resultsDiv").fadeIn(1000);
+$(document).ready(function(){
+    $("button").click(function(){
+        setWordPanel()
+    });
+});
 
-    var word = document.getElementById("word").value;
-    var url = "/words/" + word;
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = processRequest;
-    xhr.open("GET", url, true);
-    xhr.send();
+function setWordPanel(){
+    var letters = document.getElementById("letters").value;
+    var url = "/words/" + letters;
 
-    function processRequest(e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var words = JSON.parse(xhr.responseText)["words"];
-            var word = document.getElementById("word").value;
-            document.getElementById("panel-title").innerHTML = getPanelTitle(words.length);
-            document.getElementById('resultsDiv').style.display = "block";
-            document.getElementById("list-of-words").innerHTML = getHTMLList(words)
-         }
-    }
+    $.getJSON(url, setWordPanelFurther);
+}
+
+function setWordPanelFurther(response){
+    var words = response['words'];
+    panelTitle = getPanelTitle(words.length)
+    listOfWords = getHTMLList(words)
+    var panelId = new Date().getTime()
+    var collapseId = panelId +1
+    wordPanel = getWordPanel(panelTitle, listOfWords, panelId, collapseId);
+
+    $("#word-panels").append(wordPanel);
+
+    $(function(){
+    $("#" + collapseId).on('click',function(){
+        $("#" + panelId).remove();
+    })
+})
+}
+
+function getWordPanel(panelTitle, listOfWords, panelId, collapseId){
+    var toggleId = collapseId + 1;
+    return '<div id="'+panelId+'" class="container" style="width:320px;">\
+            <div class="panel-group">\
+                <div class="panel panel-default">\
+                    <div class="panel-heading">\
+                        <h4 class="panel-title">\
+                            <a data-toggle="collapse" href="#'+toggleId+'">'+panelTitle+'</a>\
+                             <span id="'+collapseId+'" class="pull-right" data-effect="slideUp"><i class="fa fa-times"></i></span>\
+                         </h4>\
+                     </div>\
+                     <div id="'+toggleId+'" class="panel-collapse collapse">\
+                        <ul class="list-group">'+listOfWords+'</ul>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>'
 }
 
 function getPanelTitle(numWords){
-    var word = document.getElementById("word").value;
-    return "Words for  " + word + "<span id='badge' class='badge'> "+ numWords +"</span>";
-    }
+    var letters = document.getElementById("letters").value;
+    return letters + "<span id='badge' class='badge'> "+ numWords +" words</span>";
+}
 
 function getHTMLList(array) {
     var listElements = ""
     for (var i = 0; i < array.length; i++) {
         listElements += "<li class='list-group-item'>" + array[i] + "</li>"
     }
-    return "<ul>" + listElements + "</ul>";
+    return listElements
 }
 
