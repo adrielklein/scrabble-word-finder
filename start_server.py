6c24e16e95387e577c16bae6dc1a81c96b2fb63a
Khuyen Duong <truckhuyen@gmail.com>
2018-06-14 02:21:55 -0700

    Game extraction in progress

--- start_server.py
+++ start_server.py
@@ -1,14 +1,24 @@
 import os
+import autopy
+import datetime
+from PIL import Image
+import pytesseract
 
 from app.main import create_app
-
 PORT = 5000
-
 
 def start_server():
     app = create_app()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', PORT)))
 
+def capture_screen():
+    b = autopy.bitmap.capture_screen()
+    print(b)
+    image_name = "./screenshot" + str(datetime.datetime.now()) + ".png"
+    b.save(image_name)
+    print (pytesseract.image_to_string(Image.open(image_name)))
 
 if __name__ == '__main__':
+    # capture_screen()
     start_server()
+    
